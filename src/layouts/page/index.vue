6ca966552e974636f6d8fd206fd3f71585044464
ZhangYantao <zhangyantao@rivergame.net>
2022-02-28 10:43:02 +0800

    fix(router): 修复页面缓存逻辑，去掉自带的include方式，添加routemeta中配置的缓存逻辑

--- src/layouts/page/index.vue
+++ src/layouts/page/index.vue
@@ -14,7 +14,7 @@
         mode="out-in"
         appear
       >
-        <keep-alive v-if="openCache" :include="getCaches">
+        <keep-alive v-if="canOpenCache(route)">
           <component :is="Component" :key="route.fullPath" />
         </keep-alive>
         <component v-else :is="Component" :key="route.fullPath" />
@@ -36,33 +36,53 @@
   import { getTransitionName } from './transition';
 
   import { useMultipleTabStore } from '/@/store/modules/multipleTab';
+  import { RouteLocationNormalized } from 'vue-router';
 
   export default defineComponent({
     name: 'PageLayout',
     components: { FrameLayout },
     setup() {
       const { getShowMultipleTab } = useMultipleTabSetting();
-      const tabStore = useMultipleTabStore();
+      // const tabStore = useMultipleTabStore();
 
       const { getOpenKeepAlive, getCanEmbedIFramePage } = useRootSetting();
 
       const { getBasicTransition, getEnableTransition } = useTransitionSetting();
 
-      const openCache = computed(() => unref(getOpenKeepAlive) && unref(getShowMultipleTab));
+      const openCache = computed(() => {
+        if (unref(getOpenKeepAlive) && unref(getShowMultipleTab)) {
+          //缓存页面大前提允许
+          return true;
+        }
+        return false;
+      });
 
       const getCaches = computed((): string[] => {
+        debugger;
         if (!unref(getOpenKeepAlive)) {
           return [];
         }
-        return tabStore.getCachedTabList;
+        const list = useMultipleTabStore().getCachedTabList;
+        return list;
       });
-
+      function canOpenCache(route: RouteLocationNormalized) {
+        if (openCache.value) {
+          //缓存页面大前提允许
+          if (route.meta.ignoreKeepAlive) {
+            return false;
+          }
+          return true;
+        } else {
+          return false;
+        }
+      }
       return {
         getTransitionName,
         openCache,
+        getCaches,
+        canOpenCache,
         getEnableTransition,
         getBasicTransition,
-        getCaches,
         getCanEmbedIFramePage,
       };
     },
