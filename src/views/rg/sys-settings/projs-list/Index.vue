57b48d016cc19f75d016236594245843456a0d44
ZhangYantao <zhangyantao@rivergame.net>
2022-03-06 21:19:41 +0800

    feat: 项目列表管理页面逻辑实现

--- src/views/rg/sys-settings/projs-list/Index.vue
+++ src/views/rg/sys-settings/projs-list/Index.vue
@@ -1,12 +1,156 @@
-<template> <span>SysProjsListMg</span></template>
+<template>
+  <PageWrapper dense contentFullHeight :class="`${prefixCls}`">
+    <div class="w-full h-full space-y-5px">
+      <a-card v-if="useSearchState" class="w-full rg-antd-cart" hoverable>
+        <BasicForm @register="formRegisterFn" />
+      </a-card>
+      <BasicTable @register="registerTableFn">
+        <template #tableTitle>
+          <span class="flex space-x-3">
+            <span class="font-bold">项目列表</span>
+            <TableAction
+              :divider="false"
+              :actions="[
+                {
+                  tooltip: {
+                    title: '新建项目',
+                    placement: 'top',
+                  },
+                  type: 'primary',
+                  size: 'small',
+                  shape: 'circle',
+                  icon: 'bx:comment-add',
+                  onClick: addBtnClickFn.bind(null),
+                },
+              ]"
+            />
+          </span>
+        </template>
+        <template #toolbar>
+          <GzShowSearchFormBtn
+            :prop-show-state="useSearchState"
+            :prop-toggle-show-state="useSearchBtnEventFn"
+          />
+        </template>
+      </BasicTable>
+    </div>
+  </PageWrapper>
+</template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
+  import { PageWrapper } from '/@/components/Page';
+  import { useDesign } from '/@/hooks/web/useDesign';
+  import { BasicForm, useForm } from '/@/components/Form';
+  import { BasicTable, SorterResult, TableAction, useTable } from '/@/components/Table';
+  import {
+    getProjsColumnsCfg,
+    projectsList,
+    getSearchFormCfg,
+  } from '/@/views/rg/sys-settings/projs-list/inner/projs.data';
+  import { arrSortFn } from '/@/utils/arrayUtils';
+  import GzShowSearchFormBtn from '/@/components/GzShowSearchFormBtn';
 
-  defineComponent({
+  export default defineComponent({
     name: 'SysProjsListMg',
-    setup() {},
+    components: {
+      PageWrapper,
+      BasicTable,
+      GzShowSearchFormBtn,
+      BasicForm,
+      TableAction,
+    },
+    setup() {
+      //样式表当前页面根元素
+      const { prefixCls } = useDesign('sys-projs-mg');
+      //#region form =================================
+
+      const [formRegisterFn] = useForm({
+        labelWidth: '80px',
+        size: 'small',
+        showAdvancedButton: true,
+        schemas: getSearchFormCfg,
+        actionColOptions: {
+          span: 24,
+        },
+        advancedBtnEventFunc: () => {
+          tableMethods.redoHeight();
+        },
+        resetButtonOptions: {
+          size: 'small',
+        },
+        submitButtonOptions: {
+          size: 'small',
+        },
+      });
+      //#endregion ----------------------------------
+      //#region table data =================================
+      const useSearchState = ref<boolean>(false);
+      let tableDatas: any[] = [];
+      let isSorting = false;
+      //#endregion -----------------------------------------
+      //#region table =================================
+      const getProjectsListApi = () => {
+        return new Promise<any[]>((resolve, reject) => {
+          if (true) {
+            if (isSorting) {
+              isSorting = false;
+              resolve(tableDatas);
+            } else {
+              const list = projectsList;
+              resolve(list);
+            }
+          } else {
+            reject(new Error(''));
+          }
+        });
+      };
+      const tableSortFn = (sortInfo: SorterResult) => {
+        console.log('tableSortFn', sortInfo);
+        isSorting = true;
+        const list = tableMethods.getDataSource().slice();
+        const sortList = arrSortFn(list, sortInfo.field, sortInfo.order);
+        tableDatas = sortList;
+      };
+      const useSearchBtnEventFn = () => {
+        useSearchState.value = !useSearchState.value;
+        tableMethods.redoHeight();
+      };
+      const addBtnClickFn = () => {};
+      const [registerTableFn, tableMethods] = useTable({
+        title: '用户列表',
+        api: getProjectsListApi,
+        showTableSetting: true,
+        bordered: true,
+        columns: getProjsColumnsCfg,
+        canResize: true,
+        pagination: false,
+        showIndexColumn: false,
+        sortFn: tableSortFn,
+        actionColumn: {
+          width: '160px',
+          title: '操作',
+          fixed: 'right',
+          dataIndex: 'action',
+          slots: { customRender: 'colAction' },
+        },
+      });
+      //#endregion ----------------------------------
+      return {
+        prefixCls,
+        useSearchState,
+        formRegisterFn,
+        registerTableFn,
+        useSearchBtnEventFn,
+        addBtnClickFn,
+      };
+    },
   });
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  @prefix-cls: ~'@{namespace}-sys-users-mg';
+  @ns-prefix: ~'@{namespace}';
+  .@{prefix-cls} {
+  }
+</style>
