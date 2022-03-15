879f176aa7aa6842a5de85ea9d1d8ce0d5baa00c
ZhangYantao <zhangyantao@rivergame.net>
2022-03-15 15:48:54 +0800

    feat: 项目配置修改  个人项目列表页面修改

--- src/views/rg/sys-mine/user-projects/SysUserProjsView.vue
+++ src/views/rg/sys-mine/user-projects/SysUserProjsView.vue
@@ -4,13 +4,114 @@
  * @Date         : 2022/2/25
  * @FilePath     : src/views/rg/sys-mine/SysUserProjsView.vue
 -->
-<template> <div> SysUserProjsView</div></template>
+<template>
+  <PageWrapper dense contentFullHeight :class="`${prefixCls}`">
+    <div class="w-full h-full space-y-5px">
+      <!--      <a-card v-if="useSearchState" class="w-full rg-antd-cart" hoverable>-->
+      <!--        <BasicForm @register="formRegisterFn" />-->
+      <!--      </a-card>-->
+      <BasicTable @register="registerTableFn">
+        <template #tableTitle>
+          <span class="flex space-x-3">
+            <span class="font-bold">项目列表</span>
+          </span>
+        </template>
+        <template #toolbar>
+          <!--          <GzShowSearchFormBtn-->
+          <!--            :prop-show-state="useSearchState"-->
+          <!--            :prop-toggle-show-state="useSearchBtnEventFn"-->
+          <!--          />-->
+        </template>
+        <template #colCreateTime="{ record, column }">
+          <span>{{ tableRowFormatFn(record, column) }}</span>
+        </template>
+        <template #colAction="{ record }">
+          <TableAction
+            :actions="[
+              {
+                icon: 'clarity:note-edit-line',
+                // onClick: tableHandleEdit.bind(null, record),
+                tooltip: '编辑',
+              },
+              {
+                icon: 'icon-park-outline:lock',
+                color: 'error',
+                ifShow: record.state === 0,
+                tooltip: '锁定项目',
+                popConfirm: {
+                  title: '是否锁定项目',
+                  // confirm: tableHandleLockFn.bind(null, record),
+                },
+              },
+              {
+                icon: 'mdi:lock-open-variant-outline',
+                // color: 'error',
+                ifShow: record.state !== 0,
+                tooltip: '解锁项目',
+                popConfirm: {
+                  title: '是否解锁项目',
+                  // confirm: tableHandleUnlockFn.bind(null, record),
+                },
+              },
+            ]"
+          />
+        </template>
+      </BasicTable>
+    </div> </PageWrapper
+></template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import { BasicTable, SorterResult, TableAction, useTable } from '/@/components/Table';
+  import { getProjsColumnsCfg } from '/@/views/rg/sys-settings/projs-list/inner/projs.data';
+  import { log } from '/@/utils/log';
+  import { arrSortFn } from '/@/utils/arrayUtils';
+  import { projectsList } from '/@/views/rg/sys-mine/user-projects/inner/userProjs.data';
 
   export default defineComponent({
     name: 'SysUserProjsView',
+    components: {
+      BasicTable,
+      TableAction,
+    },
+    setup() {
+      const tableRowFormatFn = (record, column) => {
+        return record[column.dataIndex];
+      };
+      const tableSortFn = (sortInfo: SorterResult) => {
+        log('tableSortFn', sortInfo);
+        const list = tableMethods.getRawDataSource().slice();
+        const sortList = arrSortFn(list, sortInfo.field, sortInfo.order);
+        tableMethods.setTableData(sortList);
+      };
+      const getMyProjsApi = () => {
+        return new Promise((resolve) => {
+          resolve(projectsList);
+        });
+      };
+      const [registerTableFn, tableMethods] = useTable({
+        title: '用户列表',
+        api: getMyProjsApi,
+        showTableSetting: true,
+        bordered: true,
+        columns: getProjsColumnsCfg,
+        canResize: true,
+        pagination: false,
+        showIndexColumn: false,
+        sortFn: tableSortFn,
+        // actionColumn: {
+        //   width: '160px',
+        //   title: '操作',
+        //   fixed: 'right',
+        //   dataIndex: 'action',
+        //   slots: { customRender: 'colAction' },
+        // },
+      });
+      return {
+        registerTableFn,
+        tableRowFormatFn,
+      };
+    },
   });
 </script>
 
---
+++ src/views/rg/sys-mine/user-projects/inner/userProjs.data.ts
@@ -0,0 +1,139 @@
+import { BasicColumn } from '/@/components/Table';
+
+export const getMyProjsColumnsCfg: BasicColumn[] = [
+  {
+    title: '项目id',
+    dataIndex: 'project_id',
+    width: '150px',
+    slots: { customRender: 'colProjectsId' },
+    sorter: true,
+  },
+  {
+    title: '项目名称',
+    dataIndex: 'project_name',
+    width: '150px',
+    sorter: true,
+    slots: { customRender: 'colProjectName' },
+  },
+  {
+    title: '项目顺序',
+    dataIndex: 'order_num',
+    width: '120px',
+    sorter: true,
+    slots: {
+      customRender: 'colOrderNum',
+    },
+  },
+  {
+    title: '项目状态',
+    dataIndex: 'state',
+    width: '120px',
+    sorter: true,
+    slots: {
+      customRender: 'colState',
+    },
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'created_at',
+    width: '120px',
+    sorter: true,
+    slots: {
+      customRender: 'colCreateTime',
+    },
+  },
+];
+
+export const projectsList = [
+  {
+    project_id: 'proj_ceshi2',
+    project_name: '测试20',
+    game_name: '测试20',
+    order_num: 0,
+    state: 0,
+    created_at: 1642688356,
+    updated_at: 1642746432,
+  },
+  {
+    project_id: 'proj_ceshi1',
+    project_name: '测试1',
+    game_name: '测试123',
+    order_num: 5,
+    state: 1,
+    created_at: 1642679835,
+    updated_at: 1642688377,
+  },
+  {
+    project_id: 'proj_z5',
+    project_name: 'Zgrils5',
+    game_name: 'Zgrils5',
+    order_num: 0,
+    state: 0,
+    created_at: 1640662331,
+    updated_at: 1640662331,
+    roleObj: {
+      roleList: [
+        {
+          role: 'operater',
+          name: '运营',
+        },
+        {
+          role: 'marketer',
+          name: '市场',
+        },
+        {
+          role: 'videorediter',
+          name: '视频制作',
+        },
+        {
+          role: 'topmanager',
+          name: '超级管理员',
+        },
+        {
+          role: 'root_role',
+          name: 'ADMIN',
+        },
+      ],
+      roleMap: {
+        operater: {
+          role: 'operater',
+          name: '运营',
+        },
+        marketer: {
+          role: 'marketer',
+          name: '市场',
+        },
+        videorediter: {
+          role: 'videorediter',
+          name: '视频制作',
+        },
+        topmanager: {
+          role: 'topmanager',
+          name: '超级管理员',
+        },
+        root_role: {
+          role: 'root_role',
+          name: 'ADMIN',
+        },
+      },
+    },
+  },
+  {
+    project_id: 'proj_topwar',
+    project_name: 'Topwar',
+    game_name: 'Topwar',
+    order_num: 0,
+    state: 0,
+    created_at: 1640662117,
+    updated_at: 1640662117,
+  },
+  {
+    project_id: 'base',
+    project_name: '初始项目',
+    game_name: '初始项目',
+    order_num: 0,
+    state: 0,
+    created_at: 1632827287,
+    updated_at: 1632827287,
+  },
+];
