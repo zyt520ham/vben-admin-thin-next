d01f67801b53c7678f96789e15549941a39a1253
ZhangYantao <zhangyantao@rivergame.net>
2022-04-01 10:42:16 +0800

    feat: 新增line上传接口

--- src/api/sys/costUploadApi.ts
+++ src/api/sys/costUploadApi.ts
@@ -1,6 +1,6 @@
 import { doBaseUploadApi } from '/@/utils/http/NetworkEngine';
 
-import { IReqUploadWxCost } from '/@/api/sys/model/uploadCostModel';
+import { IReqUploadLineCost, IReqUploadWxCost } from '/@/api/sys/model/uploadCostModel';
 import { ApiEnum } from '/@/api/ApiPath';
 
 /**
@@ -8,3 +8,8 @@ import { ApiEnum } from '/@/api/ApiPath';
  */
 export const uploadWxCostApi = (params: IReqUploadWxCost) =>
   doBaseUploadApi<any>(ApiEnum.UploadWxCost, params, () => {});
+/**
+ * @description: line花费上传
+ */
+export const uploadLineCostApi = (params: IReqUploadLineCost) =>
+  doBaseUploadApi<any>(ApiEnum.UploadLineCost, params, () => {});
--- src/api/sys/model/uploadCostModel.ts
+++ src/api/sys/model/uploadCostModel.ts
@@ -1,10 +1,25 @@
-export interface IReqUploadWxCost {
-  app?: string;
-  account: string;
-  day: string;
+/** 上传请求参数base*/
+
+export interface IReqUploadBase {
+  /** 文件 */
   file: File | Blob;
-  // file name
+  /** 多文件*/
+  files?: File[] | Blob[];
+  /** 文件名 */
   filename?: string;
-  name?: string;
-  data?: any;
+}
+/** 微信上传请求参数 */
+export interface IReqUploadWxCost extends IReqUploadBase {
+  /** 项目APPID */
+  app?: string;
+  /** wx账户*/
+  account: string;
+  /** 花费日期*/
+  day: string;
+}
+export interface IReqUploadLineCost extends IReqUploadBase {
+  /** 项目APPID */
+  app?: string;
+  /** 花费上传时间*/
+  day: string;
 }
